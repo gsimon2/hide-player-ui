bae30e1fc2b18a1ededbf80408d2afc440fe1358
Glen Simon <glen.a.simon@gmail.com>
2020-12-06 15:15:34 -0500

    Add module

---
+++ scripts/hide-player-ui.js
@@ -0,0 +1,17 @@
+Hooks.on('init', () => {
+    game.settings.register("hide-player-ui", "hidePlayerUI", {
+        name: "Hide Player UI",
+        hint: "Enable this to hide all control UI for Players so that they only see the map",
+        scope: "world",
+        config: true,
+        default: true,
+        type: Boolean,
+    });
+});
+
+Hooks.on('canvasInit', () => {
+    if (game.user.isGM === false && game.settings.get('hide-player-ui', 'hidePlayerUI')) {
+        var rootElement = document.getElementsByClassName('vtt game system-pf2e')[0];
+        rootElement.classList.add('hide-player-ui');
+    }
+})
