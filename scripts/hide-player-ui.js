5cec5ce6b3d35e4bbcadb0aa24f258c80f2698d5
Glen Simon <glen.a.simon@gmail.com>
2022-10-06 19:09:19 -0400

    Verify foundry v10 and format scripts

--- scripts/hide-player-ui.js
+++ scripts/hide-player-ui.js
@@ -1,142 +1,208 @@
-import {registerSettings} from './settings.js';
+import { registerSettings } from "./settings.js";
 
-Hooks.on('init', () => {
-    registerSettings();
+Hooks.on("init", () => {
+   registerSettings();
 });
 
-Hooks.on('ready', () => {
-    const playerName = game.user.data.name;
-    var hiddenPlayersList = [];
+Hooks.on("ready", () => {
+   const playerName = game.user.data.name;
+   var hiddenPlayersList = [];
 
-    if (!game.settings.get('hide-player-ui', 'hideForAllPlayers')) {
-        try {
-            hiddenPlayersList = game.settings.get('hide-player-ui', 'hiddenPlayers').split(',');
-        } catch (e) {
-            console.error('hide-player-ui: Failed to parse list of players to hide UI for!', e);
-        }
-    }
+   if (!game.settings.get("hide-player-ui", "hideForAllPlayers")) {
+      try {
+         hiddenPlayersList = game.settings
+            .get("hide-player-ui", "hiddenPlayers")
+            .split(",");
+      } catch (e) {
+         console.error(
+            "hide-player-ui: Failed to parse list of players to hide UI for!",
+            e
+         );
+      }
+   }
 
-    const isPlayerUiOverridden = game.user.isGM === false && (game.settings.get('hide-player-ui', 'hideForAllPlayers') || hiddenPlayersList.includes(playerName));
-    const settings = game.settings.get('hide-player-ui', 'settings');
-    const playerConfig = game.settings.get('hide-player-ui', 'playerConfig');
+   const isPlayerUiOverridden =
+      game.user.isGM === false &&
+      (game.settings.get("hide-player-ui", "hideForAllPlayers") ||
+         hiddenPlayersList.includes(playerName));
+   const settings = game.settings.get("hide-player-ui", "settings");
+   const playerConfig = game.settings.get("hide-player-ui", "playerConfig");
 
-    if (playerConfig.hideLogo || (isPlayerUiOverridden && settings.hideLogo)) {
-        hideElement('logo');
-    }
+   if (playerConfig.hideLogo || (isPlayerUiOverridden && settings.hideLogo)) {
+      hideElement("logo");
+   }
 
-    if (playerConfig.hideNavigation.complete || (isPlayerUiOverridden && settings.hideNavigation.complete)) {
-        hideElement('navigation');
-    } else {
-        (playerConfig.hideNavigation.navToggle || (isPlayerUiOverridden && settings.hideNavigation.navToggle)) && hideElement('navToggle');
-        (playerConfig.hideNavigation.sceneList || (isPlayerUiOverridden && settings.hideNavigation.sceneList)) && hideElement('sceneList');
-        (playerConfig.hideNavigation.bossBar || (isPlayerUiOverridden && settings.hideNavigation.bossBar)) && hideElement('bossBar');
-    }
+   if (
+      playerConfig.hideNavigation.complete ||
+      (isPlayerUiOverridden && settings.hideNavigation.complete)
+   ) {
+      hideElement("navigation");
+   } else {
+      (playerConfig.hideNavigation.navToggle ||
+         (isPlayerUiOverridden && settings.hideNavigation.navToggle)) &&
+         hideElement("navToggle");
+      (playerConfig.hideNavigation.sceneList ||
+         (isPlayerUiOverridden && settings.hideNavigation.sceneList)) &&
+         hideElement("sceneList");
+      (playerConfig.hideNavigation.bossBar ||
+         (isPlayerUiOverridden && settings.hideNavigation.bossBar)) &&
+         hideElement("bossBar");
+   }
 
-    if (playerConfig.hideControls || (isPlayerUiOverridden && settings.hideControls)) {
-        hideElement('controls');
-    }
+   if (
+      playerConfig.hideControls ||
+      (isPlayerUiOverridden && settings.hideControls)
+   ) {
+      hideElement("controls");
+   }
 
-    if (playerConfig.hideSideBar.complete || (isPlayerUiOverridden && settings.hideSideBar.complete)) {
-        hideElement('sidebar');
-    } else {
-        (playerConfig.hideSideBar.chatLog || (isPlayerUiOverridden && settings.hideSideBar.chatLog)) && hideElement('chatLog');
-        (playerConfig.hideSideBar.combatTracker || (isPlayerUiOverridden && settings.hideSideBar.combatTracker)) && hideElement('combatTracker');
-        (playerConfig.hideSideBar.actorsDirectory || (isPlayerUiOverridden && settings.hideSideBar.actorsDirectory)) && hideElement('actorsDirectory');
-        (playerConfig.hideSideBar.itemsDirectory || (isPlayerUiOverridden && settings.hideSideBar.itemsDirectory)) && hideElement('itemsDirectory');
-        (playerConfig.hideSideBar.journalEntries || (isPlayerUiOverridden && settings.hideSideBar.journalEntries)) && hideElement('journalEntries');
-        (playerConfig.hideSideBar.rollableTables || (isPlayerUiOverridden && settings.hideSideBar.rollableTables)) && hideElement('rollableTables');
-        (playerConfig.hideSideBar.cardStacks || (isPlayerUiOverridden && settings.hideSideBar.cardStacks)) && hideElement('cardStacks');
-        (playerConfig.hideSideBar.audioPlaylists || (isPlayerUiOverridden && settings.hideSideBar.audioPlaylists)) && hideElement('audioPlaylists');
-        (playerConfig.hideSideBar.compendiumPacks || (isPlayerUiOverridden && settings.hideSideBar.compendiumPacks)) && hideElement('compendiumPacks');
-        (playerConfig.hideSideBar.gameSettings || (isPlayerUiOverridden && settings.hideSideBar.gameSettings)) && hideElement('gameSettings');
+   if (
+      playerConfig.hideSideBar.complete ||
+      (isPlayerUiOverridden && settings.hideSideBar.complete)
+   ) {
+      hideElement("sidebar");
+   } else {
+      (playerConfig.hideSideBar.chatLog ||
+         (isPlayerUiOverridden && settings.hideSideBar.chatLog)) &&
+         hideElement("chatLog");
+      (playerConfig.hideSideBar.combatTracker ||
+         (isPlayerUiOverridden && settings.hideSideBar.combatTracker)) &&
+         hideElement("combatTracker");
+      (playerConfig.hideSideBar.actorsDirectory ||
+         (isPlayerUiOverridden && settings.hideSideBar.actorsDirectory)) &&
+         hideElement("actorsDirectory");
+      (playerConfig.hideSideBar.itemsDirectory ||
+         (isPlayerUiOverridden && settings.hideSideBar.itemsDirectory)) &&
+         hideElement("itemsDirectory");
+      (playerConfig.hideSideBar.journalEntries ||
+         (isPlayerUiOverridden && settings.hideSideBar.journalEntries)) &&
+         hideElement("journalEntries");
+      (playerConfig.hideSideBar.rollableTables ||
+         (isPlayerUiOverridden && settings.hideSideBar.rollableTables)) &&
+         hideElement("rollableTables");
+      (playerConfig.hideSideBar.cardStacks ||
+         (isPlayerUiOverridden && settings.hideSideBar.cardStacks)) &&
+         hideElement("cardStacks");
+      (playerConfig.hideSideBar.audioPlaylists ||
+         (isPlayerUiOverridden && settings.hideSideBar.audioPlaylists)) &&
+         hideElement("audioPlaylists");
+      (playerConfig.hideSideBar.compendiumPacks ||
+         (isPlayerUiOverridden && settings.hideSideBar.compendiumPacks)) &&
+         hideElement("compendiumPacks");
+      (playerConfig.hideSideBar.gameSettings ||
+         (isPlayerUiOverridden && settings.hideSideBar.gameSettings)) &&
+         hideElement("gameSettings");
 
-        const body = document.querySelector('body');
-        body.classList.add(`hide-player-ui-dynamic-sized-sidebar`);
+      const body = document.querySelector("body");
+      body.classList.add(`hide-player-ui-dynamic-sized-sidebar`);
 
-        let sidebarSettings = {};
-        for(const [key, value] of Object.entries(settings.hideSideBar)) {
-            sidebarSettings[key] = value || playerConfig.hideSideBar[key];
-        }
+      let sidebarSettings = {};
+      for (const [key, value] of Object.entries(settings.hideSideBar)) {
+         sidebarSettings[key] = value || playerConfig.hideSideBar[key];
+      }
 
-        setFocusToFirstDisplayedTab(sidebarSettings);
-    }
+      setFocusToFirstDisplayedTab(sidebarSettings);
+   }
 
-    if (playerConfig.hidePlayers || (isPlayerUiOverridden && settings.hidePlayers)) {
-        hideElement('players');
-    }
+   if (
+      playerConfig.hidePlayers ||
+      (isPlayerUiOverridden && settings.hidePlayers)
+   ) {
+      hideElement("players");
+   }
 
-    if (playerConfig.hideHotbar || (isPlayerUiOverridden && settings.hideHotbar)) {
-        hideElement('hotbar');
-    }
+   if (
+      playerConfig.hideHotbar ||
+      (isPlayerUiOverridden && settings.hideHotbar)
+   ) {
+      hideElement("hotbar");
+   }
 
-    if (playerConfig.hidePlayerConfig || (isPlayerUiOverridden && settings.hidePlayerConfig)) {
-        hideElement('player-config');
-    }
+   if (
+      playerConfig.hidePlayerConfig ||
+      (isPlayerUiOverridden && settings.hidePlayerConfig)
+   ) {
+      hideElement("player-config");
+   }
 
-    if (playerConfig.hideTokenHUD || (isPlayerUiOverridden && settings.hideTokenHUD)) {
-        hideElement('token-hud');
-    }
+   if (
+      playerConfig.hideTokenHUD ||
+      (isPlayerUiOverridden && settings.hideTokenHUD)
+   ) {
+      hideElement("token-hud");
+   }
 
-    if (game.modules.get('token-action-hud') && game.modules.get('token-action-hud').active) {
-        if (playerConfig.hideTokenActionHUD || (isPlayerUiOverridden && settings.hideTokenActionHUD)) {
-            hideElement('token-action-hud');
-        }
-    }
+   if (
+      game.modules.get("token-action-hud") &&
+      game.modules.get("token-action-hud").active
+   ) {
+      if (
+         playerConfig.hideTokenActionHUD ||
+         (isPlayerUiOverridden && settings.hideTokenActionHUD)
+      ) {
+         hideElement("token-action-hud");
+      }
+   }
 
-    if (game.modules.get('custom-hotbar') && game.modules.get('custom-hotbar').active) {
-        if (playerConfig.hideCustomHotbar || (isPlayerUiOverridden && settings.hideCustomHotbar)) {
-            const body = document.querySelector('body');
-            body.classList.add('hide-token-custom-hotbar');
-        }   
-    }
+   if (
+      game.modules.get("custom-hotbar") &&
+      game.modules.get("custom-hotbar").active
+   ) {
+      if (
+         playerConfig.hideCustomHotbar ||
+         (isPlayerUiOverridden && settings.hideCustomHotbar)
+      ) {
+         const body = document.querySelector("body");
+         body.classList.add("hide-token-custom-hotbar");
+      }
+   }
 });
 
 const hideElement = (id) => {
-    const body = document.querySelector('body');
-    body.classList.add(`hide-player-ui-${id}`);
+   const body = document.querySelector("body");
+   body.classList.add(`hide-player-ui-${id}`);
 };
 
 const setFocusToFirstDisplayedTab = (hideSideBarSettings) => {
-    if (!hideSideBarSettings.chatLog)
-    {
-        return;
-    }
+   if (!hideSideBarSettings.chatLog) {
+      return;
+   }
 
-    for(const [key, value] of Object.entries(hideSideBarSettings)) {
-        if (key === "complete") {
-            continue;
-        }
+   for (const [key, value] of Object.entries(hideSideBarSettings)) {
+      if (key === "complete") {
+         continue;
+      }
 
-        if (value === false) {
-            switch (key) {
-                case 'chatLog':
-                    document.querySelector('a[data-tab="chat"]').click();
-                    return;
-                case 'combatTracker':
-                    document.querySelector('a[data-tab="combat"]').click();
-                    return;
-                case 'actorsDirectory':
-                    document.querySelector('a[data-tab="actors"]').click();
-                    return;
-                case 'itemsDirectory':
-                    document.querySelector('a[data-tab="items"]').click();
-                    return;
-                case 'journalEntries':
-                    document.querySelector('a[data-tab="journal"]').click();
-                    return;
-                case 'rollableTables':
-                    document.querySelector('a[data-tab="tables"]').click();
-                    return;
-                case 'audioPlaylists':
-                    document.querySelector('a[data-tab="playlists"]').click();
-                    return;
-                case 'compendiumPacks':
-                    document.querySelector('a[data-tab="compendium"]').click();
-                    return;
-                case 'gameSettings':
-                    document.querySelector('a[data-tab="settings"]').click();
-                    return;
-            };
-        }
-    }
-}
+      if (value === false) {
+         switch (key) {
+            case "chatLog":
+               document.querySelector('a[data-tab="chat"]').click();
+               return;
+            case "combatTracker":
+               document.querySelector('a[data-tab="combat"]').click();
+               return;
+            case "actorsDirectory":
+               document.querySelector('a[data-tab="actors"]').click();
+               return;
+            case "itemsDirectory":
+               document.querySelector('a[data-tab="items"]').click();
+               return;
+            case "journalEntries":
+               document.querySelector('a[data-tab="journal"]').click();
+               return;
+            case "rollableTables":
+               document.querySelector('a[data-tab="tables"]').click();
+               return;
+            case "audioPlaylists":
+               document.querySelector('a[data-tab="playlists"]').click();
+               return;
+            case "compendiumPacks":
+               document.querySelector('a[data-tab="compendium"]').click();
+               return;
+            case "gameSettings":
+               document.querySelector('a[data-tab="settings"]').click();
+               return;
+         }
+      }
+   }
+};
--- scripts/settings-form.js
+++ scripts/settings-form.js
@@ -1,42 +1,52 @@
-import {defaultSettings} from './settings.js';
+import { defaultSettings } from "./settings.js";
 
 export class HidePlayerUISettingsForm extends FormApplication {
-    static get defaultOptions() {
-        return mergeObject(super.defaultOptions, {
-            title: game.i18n.localize('hide-player-ui.settings-form.hide-connecting-players-ui.title'),
-            id: 'hide-player-ui-settings-form',
-            template: 'modules/hide-player-ui/templates/settings-form.html',
-            width: 500,
-            closeOnSubmit: true
-        });
-    }
+   static get defaultOptions() {
+      return mergeObject(super.defaultOptions, {
+         title: game.i18n.localize(
+            "hide-player-ui.settings-form.hide-connecting-players-ui.title"
+         ),
+         id: "hide-player-ui-settings-form",
+         template: "modules/hide-player-ui/templates/settings-form.html",
+         width: 500,
+         closeOnSubmit: true,
+      });
+   }
 
-    storedData = game.settings.get('hide-player-ui', 'settings');
+   storedData = game.settings.get("hide-player-ui", "settings");
 
-    getData(options) {
-        const moduleSpecificData = {
-            renderTokenActionHudOption: game.modules.get('token-action-hud') && game.modules.get('token-action-hud').active,
-            renderCustomHotbarOption: game.modules.get('custom-hotbar') && game.modules.get('custom-hotbar').active,
-            renderBossBarOption: game.modules.get('bossbar') && game.modules.get('bossbar').active
-        };
+   getData(options) {
+      const moduleSpecificData = {
+         renderTokenActionHudOption:
+            game.modules.get("token-action-hud") &&
+            game.modules.get("token-action-hud").active,
+         renderCustomHotbarOption:
+            game.modules.get("custom-hotbar") &&
+            game.modules.get("custom-hotbar").active,
+         renderBossBarOption:
+            game.modules.get("bossbar") && game.modules.get("bossbar").active,
+      };
 
-        const data = mergeObject(moduleSpecificData, this.storedData);
-        return data;
-    }
+      const data = mergeObject(moduleSpecificData, this.storedData);
+      return data;
+   }
 
-    activateListeners(html) {
-        super.activateListeners(html);
-        html.find('button[name="reset"]').click(this.onReset.bind(this));
-    }
+   activateListeners(html) {
+      super.activateListeners(html);
+      html.find('button[name="reset"]').click(this.onReset.bind(this));
+   }
 
-    onReset() {
-        this.storedData = JSON.parse(JSON.stringify(defaultSettings));
+   onReset() {
+      this.storedData = JSON.parse(JSON.stringify(defaultSettings));
 
-        this.render();
-    }
+      this.render();
+   }
 
-    _updateObject(events, formData) {
-        let settings = mergeObject(this.storedData, formData, {insertKeys: true, insertValues: true});
-        game.settings.set('hide-player-ui', 'settings', settings);
-    }
-};
+   _updateObject(events, formData) {
+      let settings = mergeObject(this.storedData, formData, {
+         insertKeys: true,
+         insertValues: true,
+      });
+      game.settings.set("hide-player-ui", "settings", settings);
+   }
+}
