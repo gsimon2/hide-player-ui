9a3f242081de10562f4f1fa0a8edebfe2173f485
Joel Plourde <100144761+joelplourde4@users.noreply.github.com>
2024-08-04 16:23:27 -0400

    Fix an issue where the GM would be considered a player.

--- scripts/isGM.js
+++ scripts/isGM.js
@@ -1,5 +1,21 @@
 export const isGmOrAssistant = () => {
+    // If the user is the GM, then automatically return, no need to check any settings.
+    if (game.user.isGM) {
+        return true;
+    }
+
+    // The user might be an assistant, but the GM might want to hide menus from the assistant
     const hideForAssistantGM = game.settings.get("hide-player-ui", "hideForAssistantGM");
+    if (hideForAssistantGM) {
+        return false;
+    }
+
+    // Else If the user is an assistant, all good.
     const isUserAssistant = game.user.hasRole('ASSISTANT');
-    return game.user.isGM && (!hideForAssistantGM || !isUserAssistant);
+    if (isUserAssistant) {
+        return true;
+    }
+
+    // Else, it is a player.
+    return false;
 }
