8cf657852fd4f464842eaca805948e87fc09910a
Joel Plourde <100144761+joelplourde4@users.noreply.github.com>
2024-08-04 16:27:30 -0400

    Update isGM.js

--- scripts/isGM.js
+++ scripts/isGM.js
@@ -1,21 +1,15 @@
 export const isGmOrAssistant = () => {
-    // If the user is the GM, then automatically return, no need to check any settings.
     if (game.user.isGM) {
         return true;
     }
 
-    // The user might be an assistant, but the GM might want to hide menus from the assistant
-    const hideForAssistantGM = game.settings.get("hide-player-ui", "hideForAssistantGM");
-    if (hideForAssistantGM) {
+    if (game.settings.get("hide-player-ui", "hideForAssistantGM")) {
         return false;
     }
 
-    // Else If the user is an assistant, all good.
-    const isUserAssistant = game.user.hasRole('ASSISTANT');
-    if (isUserAssistant) {
+    if (game.user.hasRole('ASSISTANT')) {
         return true;
     }
 
-    // Else, it is a player.
     return false;
 }
