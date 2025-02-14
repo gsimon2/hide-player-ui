02ec93f4cef93edf611600999f368e320e6a723a
Glen Simon <glen.a.simon@gmail.com>
2025-02-13 20:59:13 -0500

    Fix hiding elements for assistant GM

--- scripts/isGM.js
+++ scripts/isGM.js
@@ -1,10 +1,11 @@
 export const isGmOrAssistant = () => {
-    if (game.user.isGM) {
+    if (game.user.isGM && game.user.hasRole('GAMEMASTER')) {
         return true;
     }
 
     const hideForAssistantGM = game.settings.get("hide-player-ui", "hideForAssistantGM");
     const isUserAssistant = game.user.hasRole('ASSISTANT');
+
     if (hideForAssistantGM && isUserAssistant) {
         return false;
     }
